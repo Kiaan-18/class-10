5b04f7cd5ac7733e1d63ae95895453ceb03e6fd7
Kiaan-18 <kapse.kiaan@gmail.com>
2025-04-13 11:07:57 +0200

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number:"))
+sum=0
+for i in range(1,num+1):
+    sum=sum+i
+    print("the sum of natural number is",sum)
---
+++ activity2.py
@@ -0,0 +1,5 @@
+string=input("Enter a word" )
+string2=("")
+for i in string :
+    string2=i+string2
+print("The reversed String is",string2)
